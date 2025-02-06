318550bd8819e042110959b126164fbd8c82223d
EwertonSA <ewerton.60@hotmail.com>
2025-02-06 11:44:17 -0300

    Relacionamento entre modelos.

---
+++ src/adminjs/resources/index.ts
@@ -0,0 +1,18 @@
+// src/adminjs/resources/index.ts
+
+import { ResourceWithOptions } from "adminjs";
+import { Category } from "../../models";
+import { categoryResourceOptions } from "./category";
+import { Course } from "../../models/Course";
+import { courseResourceOptions } from "./course";
+
+export const adminJsResources: ResourceWithOptions[] = [
+  {
+    resource: Category,
+    options: categoryResourceOptions
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions
+  },
+]
