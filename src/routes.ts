e680444348b787c492553385c1f5f75acc588e37
EwertonSA <ewerton.60@hotmail.com>
2025-02-11 09:46:08 -0300

    Resolvendo problemas com middleware de autenticação.

--- src/routes.ts
+++ src/routes.ts
@@ -3,18 +3,19 @@ import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/courseController'
 import { episodesController } from './controllers/episodeController'
 import { authController } from './controllers/authController'
-import { ensureAuthViaQuery } from './middlwares/auth'
+import { ensureauth, ensureAuthViaQuery } from './middlwares/auth'
 
 export const router = express.Router()
 router.post('/auth/login', authController.login)
 router.post('/auth/register',authController.register)
-router.get('/categories', categoriesController.index)
-router.get('/categories/:id', categoriesController.show)
 
-router.get('/courses/featured', coursesController.featured)
+router.get('/categories',ensureauth ,categoriesController.index)
+router.get('/categories/:id',ensureauth ,categoriesController.show)
+
+router.get('/courses/featured',ensureauth ,coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', coursesController.search)
-router.get('/courses/:id',coursesController.show)
+router.get('/courses/search',ensureauth ,coursesController.search)
+router.get('/courses/:id', ensureauth,coursesController.show)
 
 router.get("/episodes/stream",episodesController.stream)
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
