13136b076fc2a701987d7a699a032b237835eb1b
EwertonSA <ewerton.60@hotmail.com>
2025-02-13 09:22:50 -0300

    Resolvendo problemas com PORT no deploy.

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,12 @@ app.use(adminjs.options.rootPath,adminjsRouter)
 app.use(express.json())
 app.use(express.static('public'))
 app.use(router)
-const PORT =process.env.PORT || 3000
-app.listen(PORT,()=>{
-    database.authenticate().then(()=>{
-        console.log( 'DB connection successfull')
-    })
-    console.log(`server started seccessfuly at port ${PORT}`)
-})
+
+const PORT = process.env.PORT || 3000;  
+app.listen(PORT, () => {
+    database.authenticate()
+        .then(() => console.log('DB connection successful'))
+        .catch(err => console.error('Database connection error:', err));
+
+    console.log(`Server started successfully at port ${PORT}`);
+});
