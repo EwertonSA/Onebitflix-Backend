3125fdb38bbbf3b437c66bbeabe69ce7d1b1310f
EwertonSA <ewerton.60@hotmail.com>
2025-02-13 16:43:40 -0300

    Banco conectado.

--- src/config/sequelizeCli.js
+++ src/config/sequelizeCli.js
@@ -10,11 +10,17 @@ module.exports = {
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true, // Necessário para garantir que a conexão será feita via SSL
+        require: false, // Necessário para garantir que a conexão será feita via SSL
         rejectUnauthorized: false, // Defina como "false" se o certificado não for verificado (comum em alguns bancos)
       },
     },
   },
 };
-
+client.connect()
+  .then(() => {
+    console.log("Conectado ao banco de dados!");
+  })
+  .catch((err) => {
+    console.error("Erro de conexão", err.stack);
+  });
 console.log(process.env.DATABASE_URL);
