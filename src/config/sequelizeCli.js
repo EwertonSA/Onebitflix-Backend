1d7bd2c827601e086d52ad0a9b624d2ae68683b1
EwertonSA <ewerton.60@hotmail.com>
2025-02-13 14:07:52 -0300

    Subindo migrations e seeders para o DB do deploy.

--- src/config/sequelizeCli.js
+++ src/config/sequelizeCli.js
@@ -1,15 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
 
-module.exports={
-    development:{
-      url:process.env.DATABASE_URL,
-      dialect:'postgres'
-
-,
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // Necessário para garantir que a conexão será feita via SSL
+        rejectUnauthorized: false, // Defina como "false" se o certificado não for verificado (comum em alguns bancos)
+      },
     },
-    production:{
-        url:process.env.DATABASE_URL,
-        dialect:'postgres'
-    }
-    }
-    console.log(process.env.DATABASE_URL);
+  },
+};
+
+console.log(process.env.DATABASE_URL);
