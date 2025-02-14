8b58b8e70b2608b5c806ec973f93ab91e93b8378
EwertonSA <ewerton.60@hotmail.com>
2025-02-14 11:25:55 -0300

    Adicionar campo para armazenar link do YouTube em episódios

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,29 +1,35 @@
-// src/adminjs/resources/episode.ts
+
 
 import path from 'path'
 import uploadFileFeature from '@adminjs/upload'
 import { FeatureType, ResourceOptions } from 'adminjs'
 
+
 export const episodeResourceOptions: ResourceOptions = {
   navigation: 'Catálogo',
-  editProperties: ['name', 'synopsis', 'courseId', 'order', 'uploadVideo', 'secondsLong'  ],
+  editProperties: ['name', 'synopsis', 'courseId', 'order', 'uploadVideo', 'youtubeUrl', 'secondsLong'],
   filterProperties: ['name', 'synopsis', 'courseId', 'secondsLong', 'createdAt', 'updatedAt'],
   listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong'],
-  showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'secondsLong', 'createdAt', 'updatedAt']
+  showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'youtubeUrl', 'secondsLong', 'createdAt', 'updatedAt'],
+  properties: {
+    youtubeUrl: {
+      type: 'string',
+      isVisible: { list: true, edit: true, show: true },  // Controla a visibilidade
+    }
+  }
 }
-
 export const episodeResourceFeatures: FeatureType[] = [
   uploadFileFeature({
     provider: {
       local: {
         bucket: path.join(__dirname, '../../../uploads'),
-        opts:{}
+        opts: {}
       }
     },
     properties: {
-      key: 'videoUrl',
+      key: 'videoUrl',  // Para arquivos locais
       file: 'uploadVideo'
     },
     uploadPath: (record, filename) => `videos/course-${record.get('courseId')}/${filename}`
-  })
+  }),
 ]
